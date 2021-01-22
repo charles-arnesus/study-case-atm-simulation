1e8be77a663a5922053750a5dc47c84383f49329
Charles Arnesus <charlesarnesus.matulessy@mitrais.com>
2021-01-22 13:07:17 +0700

    add main class and Account entity

---
+++ src/com/mitrais/studycase/Main.java
@@ -0,0 +1,7 @@
+package com.mitrais.studycase;
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Study Case Charles");
+    }
+}
---
+++ src/com/mitrais/studycase/domain/entities/Account.java
@@ -0,0 +1,58 @@
+package com.mitrais.studycase.domain.entities;
+
+import java.util.Objects;
+
+public class Account {
+    private String name;
+    private String pin;
+    private String balance;
+    private String accountNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Account)) return false;
+        Account account = (Account) o;
+        return Objects.equals(getName(), account.getName()) &&
+                Objects.equals(getPin(), account.getPin()) &&
+                Objects.equals(getBalance(), account.getBalance()) &&
+                Objects.equals(getAccountNumber(), account.getAccountNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getPin(), getBalance(), getAccountNumber());
+    }
+}
